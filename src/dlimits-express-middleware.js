e98c7bf770396af852add5223194c2fd1a7d2c02
Victor R. Huerta Hernández <vhuertahnz@gmail.com>
2016-10-17 13:12:46 -0500

    Rate limit headers added in the middleware

--- src/dlimits-express-middleware.js
+++ src/dlimits-express-middleware.js
@@ -11,10 +11,20 @@ export default(tries, time, store, keyResolver, limitHandler, options) => {
         dlimits.limit(key)
           .then(record => {
             req.dlimit = record;
+
+            res.header('X-RateLimit-Limit', tries);
+            res.header('X-RateLimit-Remaining', req.dlimit.remain);
+            res.header('X-RateLimit-Reset', req.dlimit.nextCountRestart);
+
             next();
           })
           .catch(record => {
             req.dlimit = record;
+
+            res.header('X-RateLimit-Limit', tries);
+            res.header('X-RateLimit-Remaining', req.dlimit.remain);
+            res.header('X-RateLimit-Reset', req.dlimit.bannedUntil);
+
             limitHandler(req, res, next);
           });
       });
